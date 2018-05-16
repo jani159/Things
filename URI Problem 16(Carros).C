d245e5319a06ae61674ba4497a10595614f3eaf4
jani159 <janiel_amorim@hotmail.com>
2018-05-16 10:35:37 -0300

    Create URI Problem 16(Carros).C

---
+++ URI Problem 16(Carros).C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	int d, t;
+	//d= distancia entre os carros e t= tempo em minutos
+	scanf("%d",&d);
+	t = 2*d;
+	printf("%d minutos",t);
+	return 0;
+}
