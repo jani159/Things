ddc29a78d10dc01b0bd4034a45c2799c440cef5d
jani159 <janiel_amorim@hotmail.com>
2018-05-16 10:39:13 -0300

    Create URI Problem 35.C

---
+++ URI Problem 35.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+	int A,B,C,D;
+	scanf("%d%d%d%d",&A, &B, &C, &D);
+	float resto;
+	resto = A%2;
+	if(B>C){
+	if(D>A)
+	if((C+D)>(A+B))
+	if(C>0)
+    if(D>0)
+    printf("Valores aceitos");	
+}
+	else{
+	    printf("Valores nao aceitos");
+}
+	return 0;
+}
+
