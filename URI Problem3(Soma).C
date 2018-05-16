0ad21dd06c4988c00aeaa2fee8567233387eba13
jani159 <janiel_amorim@hotmail.com>
2018-05-16 10:29:29 -0300

    Create URI Problem3(Soma).C

---
+++ URI Problem3(Soma).C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+	int A, B, soma;
+	printf("Diga dois numeros A e B\n");
+	scanf("%d",&A);
+	scanf("%d",&B);
+	soma = A+B;
+	printf("A soma eh: %d \n", soma);
+	return 0;
+}
