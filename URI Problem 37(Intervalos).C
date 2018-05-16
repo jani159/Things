699194757d13469595263c76b841de9e2489f45e
jani159 <janiel_amorim@hotmail.com>
2018-05-16 10:40:30 -0300

    Create URI Problem 37(Intervalos).C

---
+++ URI Problem 37(Intervalos).C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(){
+	float num;
+	scanf("%f", &num);
+	
+	if((num >= 0) && (num <= 25)){
+		printf("Intervalo [0,25]\n");
+	if((num > 25) && (num <= 50))
+		printf("Intervalo (25,50]\n");		
+	if((num > 50) && (num <= 75))
+		printf("Intervalo (50,75]\n");
+	if((num > 75) && (num <= 100))
+		printf("Intervalo (75,100]\n");
+}
+	else{
+		printf("Fora de intervalo\n");
+	}
+return 0;
+}
