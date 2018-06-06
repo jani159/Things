82a5db0eb7848a3828b4add74c7127427990469e
jani159 <janiel_amorim@hotmail.com>
2018-06-06 08:44:51 -0300

    Update Display_ModuloIC-2.ino

--- Display_ModuloIC-2/Display_ModuloIC-2.ino
+++ Display_ModuloIC-2/Display_ModuloIC-2.ino
@@ -15,7 +15,7 @@ void loop()
   lcd.setBacklight(HIGH); //Liga a luz do display(Mesmo sem isso a mensagem aparecerá, mas a "luz" do display estará apagada)
   //Configuração padrão de display
   lcd.setCursor(0,0); //Coluna 0
-  lcd.print("Ola aluno!!"); //Mensagem no display
+  lcd.print("Ola!!"); //Mensagem no display
   lcd.setCursor(0,1); //Coluna 2
   lcd.print("Aproxime seu cartao!");
 }
